38b2330db0719c9a1c97e63aaaab804a35f5dc69
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2023-03-22 10:24:26 +0000

    started templates

---
+++ templates/common.php
@@ -0,0 +1,24 @@
+<?php
+    function output_header(){ ?>
+        <!DOCTYPE html>
+        <html lang="en-US">
+        <head>
+            <title>Tickets</title>    
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <header>
+                <h1><a href="index.php">TICKETS</a></h1>
+                <h2><a href="#">FAQ</a></h2>
+            </header>
+        <?php }
+
+    function output_footer(){ ?>
+        <footer>
+            <p>&copy; Tickets</p>
+        </footer>
+        </body>
+        </html>
+    <?php } 
+?>
