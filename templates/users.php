26b49a16d2b5409d83f68b752d651d943956c6c2
Pedro <89313201+pramadureira@users.noreply.github.com>
2023-04-15 16:17:17 +0100

    Added class Client and connected the user page to the DB

--- templates/users.php
+++ templates/users.php
@@ -1,4 +1,7 @@
 <?php
+    require_once(__DIR__ . '/../database/connection.php');
+    require_once(__DIR__ . '/../database/client.class.php');
+
     function output_users() { ?>
     
         <section id="form-manage-users">
@@ -12,83 +15,33 @@
                 </thead>
                 <tbody>
                     <?php
-
+                        $db = getDatabaseConnection();
+                        $users = Client::getAllUsers($db);
+                        foreach ($users as $user) {
+                            output_user($user);
+                        }
                     ?>
-                    <tr>
-                        <td>
-                            <div class="user_info">
-                                <img src="https://picsum.photos/40/40" alt="">
-                                <div class="username">
-                                    <p>Ronaldo</p>
-                                    <p>@ronaldinho</p>
-                                </div>
-                            </div>
-                        </td>
-                            <td>
-                                <select>
-                                    <option value="client">Client</option>
-                                    <option value="agent">Agent</option>
-                                    <option value="admin">Admin</option>
-                                </select>
-                            </td>
-                        <td>
-                            <select>
-                                <option value="hr">Human Resources</option>
-                                <option value="it">Information Technology</option>
-                                <option value="sales">Sales</option>
-                                <option value="finance">Finance</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="user_info">
-                                <img src="https://picsum.photos/40/40" alt="">
-                                <div class="username">
-                                    <p>Messi</p>
-                                    <p>@saco_do_lixo</p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <select>
-                                <option value="client">Client</option>
-                                <option value="agent">Agent</option>
-                                <option value="admin">Admin</option>
-                            </select>
-                        </td>
-                        <td>
-                            <select>
-                                <option value="hr">Human Resources</option>
-                                <option value="it">Information Technology</option>
-                                <option value="sales">Sales</option>
-                                <option value="finance">Finance</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </td>
-                    </tr>
                 </tbody>
             </table>
         </section>
     <?php }
 
-    function output_user() { ?>
+    function output_user($user) { ?>
         <tr>
             <td>
                 <div class="user_info">
                     <img src="https://picsum.photos/40/40" alt="">
                     <div class="username">
-                        <p>Ronaldo</p>
-                        <p>@ronaldinho</p>
+                        <p><?=$user->name?></p>
+                        <p>@<?=$user->username?></p>
                     </div>
                 </div>
             </td>
                 <td>
                     <select>
                         <option value="client">Client</option>
-                        <option value="agent">Agent</option>
-                        <option value="admin">Admin</option>
+                        <option value="agent" <?= (!$user->isAgent && $user->isAdmin) ? 'selected' : '' ?>>Agent</option>
+                        <option value="admin" <?= $user->isAdmin ? 'selected' : '' ?>>Admin</option>
                     </select>
                 </td>
             <td>
@@ -97,6 +50,7 @@
                     <option value="it">Information Technology</option>
                     <option value="sales">Sales</option>
                     <option value="finance">Finance</option>
+                    <option value="none" selected>None</option>
                     <option value="other">Other</option>
                 </select>
             </td>
