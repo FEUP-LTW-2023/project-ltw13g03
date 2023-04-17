249d1ae0999107da02b4378fcf2a5fea6141e58e
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2023-04-17 09:59:22 +0100

    fixed small bugs

--- pages/new_ticket.php
+++ pages/new_ticket.php
@@ -2,7 +2,7 @@
     require_once(__DIR__ . '/../templates/common.php');
     require_once(__DIR__ . '/../templates/ticket.php');
 
-    output_header();
+    output_header(true);
 
     new_ticket_form();
 
--- templates/common.php
+++ templates/common.php
@@ -17,10 +17,10 @@
         <body>
             <header>
                 <h1><a href="index.php">TICKETS</a></h1>
-                <a href="new-ticket.php"><h2>New Ticket</h2></a>
-                <a href="manage-users.php"><h2>Users</h2></a>
-                <a href="faq.php"><h2>FAQ</h2></a>
                 <?php if ($logged_in) { ?>
+                    <a href="new_ticket.php"><h2>New Ticket</h2></a>
+                    <a href="manage_users.php"><h2>Users</h2></a>
+                    <a href="faq.php"><h2>FAQ</h2></a>
                     <a id="logout" href="../actions/action_logout.php"><img src="https://freesvg.org/img/artmaster_logout_mini_icon.png" alt="logout button"></a>
                     <a id="profpic" href="profile.php"><img src="https://picsum.photos/80/80" alt=""></a>
                 <?php } ?>
