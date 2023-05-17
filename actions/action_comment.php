7bb6694a90317ff5e92b1bbcc0412ffacc58e2a1
Daniel Gago <danieljagago@gmail.com>
2023-05-17 11:55:58 +0100

    Fixed comment with no FAQ bug

--- actions/action_comment.php
+++ actions/action_comment.php
@@ -1,12 +1,18 @@
 <?php
-  session_start();
+session_start();
 
-  require_once(__DIR__ . '/../database/connection.db.php');
-  require_once(__DIR__ . '/../database/ticket.class.php');
+require_once(__DIR__ . '/../database/connection.db.php');
+require_once(__DIR__ . '/../database/ticket.class.php');
 
-  $db = getDatabaseConnection();
-  Ticket::addComment($db, $_POST['ticketId'], $_SESSION['username'], $_POST['text'], $_POST['faq']);
+$db = getDatabaseConnection();
+Ticket::addComment(
+    $db,
+    $_POST['ticketId'],
+    $_SESSION['username'],
+    $_POST['text'],
+    is_string($_POST['faq']) ? $_POST['faq'] : ''
+);
 
-  header('Location: /pages/ticket.php?id=' . $_POST['ticketId']);
+header('Location: /pages/ticket.php?id=' . $_POST['ticketId']);
 
 ?>
